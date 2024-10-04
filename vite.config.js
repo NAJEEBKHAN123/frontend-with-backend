043f92dec396c4648e646eac933a51cd98cf85aa
NAJEEBKHAN123 <najeebkhanlaku.com>
2024-10-04 10:19:44 +0500

    updated fiel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()], // Using React plugin for Vite
+  build: {
+    outDir: 'dist', // Specifies the build output directory
+  },
+});
